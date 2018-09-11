a4b8809353ddc93966515ded86483c6031982de9
MZqk <zqk_mz@163.com>
2018-09-11 12:08:45 +0800

    complete

--- gongwen.py
+++ gongwen.py
@@ -13,28 +13,30 @@ urls =eval(arealist.read()) # 转换为字典
 develop_bth = "//*[@id=\"btn_dev\"]/span/span[1]" # 开发者模式按钮
 receive_bth = "/html/body/div/div[1]/div/div[3]/a/span/span" # 接收按钮
 sure_bth = "/html/body/div[2]/div[2]/div[4]/a/span/span" # 确认接收成功按钮
+# /html/body/div[5]/div[2]/div[4]/a/span/span
 
 def openhtml(url):
     driver = drivers
-    driver.set_page_load_timeout(5) # 超时即抛出异常
+    driver.set_page_load_timeout(10) # 超时即抛出异常
     try:
-        driver.get("http://."+urls[url]+"/cztdataexchange-war/index.action")
+        driver.get("http://"+urls[url]+"/cztdataexchange-war/index.action")
         WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.XPATH, develop_bth))).click()
         sleep(0.5)
         driver.get("http://" + urls[url] + "/cztdataexchange-war/cztdataexchange/intoMyTest.action")
         WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.XPATH, receive_bth))).click()
         try:
-            WebDriverWait(driver, 4, 0.5).until(EC.presence_of_element_located((By.XPATH, sure_bth))).click()
+            WebDriverWait(driver, 10, 0.5).until(EC.presence_of_element_located((By.XPATH, sure_bth))).click()
             driver.save_screenshot(url + ".png")
             # driver.get_screenshot_as_file(url+".png")
             print(url + " 检查通过")
-        except NoSuchElementException :
+        #except NoSuchElementException :
+        except:
             driver.save_screenshot("error" + url + ".png")
             print(url + "检查不通过，请手动检查服务   http://" + urls[url] + "/cztdataexchange-war/cztdataexchange/intoMyTest.action")
         finally:
             print("----------------------------")
     except TimeoutException:
-        print(url+"拒绝访问地址，请检查网络")
+        print(url+"拒绝访问地址，请检查网络    http://"+urls[url]+"/cztdataexchange-war/index.action")
         print("XXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 
